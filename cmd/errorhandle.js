42cde71ad5d08bd2226b4381ca8fc14e5ef22528
Edgewurth <32139009+ryanedgewurth@users.noreply.github.com>
2021-04-12 22:13:12 +0100

    Fixed Issues URL

--- cmd/errorhandle.js
+++ cmd/errorhandle.js
@@ -2,5 +2,5 @@
 exports.errorhandle = function(errormsg, errorname, cmd, authorid) {
 	var date = new Date();
 	var datecurr = date.getUTCFullYear() + "-" + date.getUTCMonth() + "-" + date.getUTCDate() + " " + date.getUTCHours() + ":" + date.getUTCMinutes() + ":" + date.getUTCSeconds()
-	return "An Error Occurred, please visit https://bit.ly/2EcEqGr and create an new issue with the following as a log: ```\nDate (ISO 8601 UTC): " + datecurr + "\nFull Command Variable: " + cmd + "\nMessage Author ID: " + authorid + "\nError Name: " + errorname + "\nError Message: " + errormsg + "```";
+	return "An Error Occurred, please visit https://github.com/ryanedgewurth/flame-discord.js/issues and create an new issue with the following as a log: ```\nDate (ISO 8601 UTC): " + datecurr + "\nFull Command Variable: " + cmd + "\nMessage Author ID: " + authorid + "\nError Name: " + errorname + "\nError Message: " + errormsg + "```";
 }
